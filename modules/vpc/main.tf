9aa3a217355975570fef598dc7f9640f69bd2edf
Jacob Johnston <jacob@johnstonjacob.com>
2019-02-05 13:45:31 -0600

    add internet gateway, routing table

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -30,10 +30,16 @@ resource "aws_subnet" "homelab_subnet" {
     }
 }
 
-resource "aws_internet_gateway" "homelab_gw" {
+resource "aws_internet_gateway" "homelab_igw" {
   vpc_id = "${aws_vpc.homelab_vpc.id}"
 
   tags = {
     Name = "homelab"
   }
+}
+
+resource "aws_route" "route" {
+    route_table_id            = "${aws_vpc.homelab_vpc.default_route_table_id}"
+    destination_cidr_block    = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.homelab_igw.id}"
 }
